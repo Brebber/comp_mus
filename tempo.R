833d1a59bf18d71dcc4974e42a977411b5e7918d
Brebber <brakenhoff.brent@gmail.com>
2023-03-15 18:26:57 +0100

    Hw 11 done

--- tempo.R
+++ tempo.R
@@ -11,8 +11,8 @@ corpus <-
     metal |> mutate(genre = "Metal")
   )
 hist <-  
-  ggplot(corpus, aes(x=10*round(tempo/10))) + 
-  geom_histogram(stat="count", binwidth = 1, color = 'black', fill = 'blue') +
+  ggplot(corpus, aes(x=tempo)) + 
+  geom_histogram(binwidth = 5, color = 'black', fill = 'blue') +
   facet_wrap(~ genre) +
   theme_minimal()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
@@ -21,3 +21,12 @@ hist <-
   )
 
 ggplotly(p=hist)
+
+para <- get_tidy_audio_analysis("6LgJvl0Xdtc73RJ1mmpotq?si=cb296db6ab774280")
+para |>
+  tempogram(window_size = 8, hop_size = 1, cyclic = TRUE) |>
+  ggplot(aes(x = time, y = bpm, fill = power)) +
+  geom_raster() +
+  scale_fill_viridis_c(guide = "none") +
+  labs(x = "Time (s)", y = "Tempo (BPM)") +
+  theme_classic()
